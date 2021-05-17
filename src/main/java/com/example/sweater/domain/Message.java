c23e1a9fd5254b92e3ea9b58457b0df7da245ac3
Gorshkov Sergey <Gsn82@mail.ru>
2021-05-17 18:06:40 +0500

    4

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,67 @@
+package com.example.sweater.domain;
+
+import com.example.sweater.domain.User;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    //получаем информацию об авторе
+    @ManyToOne(fetch = FetchType.EAGER)
+    //как калонка будет называть в базе
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message() {
+    }
+
+    // если у сообщени я нет автолра , добавляем <none>
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public Message(String text, String tag, User user) {
+        this.text = text;
+        this.tag = tag;
+        this.author = user;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
