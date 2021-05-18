d0d325e5051118fb1388ac486ed3adf25e09d65c
Gorshkov Sergey <Gsn82@mail.ru>
2021-05-18 13:13:40 +0500

    7

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -3,22 +3,28 @@ package com.example.sweater.controller;
 import com.example.sweater.domain.Message;
 import com.example.sweater.domain.User;
 import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
-    private final MessageRepo messageRepo;
+    @Autowired
+    private MessageRepo messageRepo;
 
-    public MainController(MessageRepo messageRepo) {
-        this.messageRepo = messageRepo;
-    }
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -46,8 +52,25 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model) {
+            Map<String, Object> model,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            // если каталог не существует , то задаем
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs();
+            }
+            // генерируем случайное имя
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
 
         messageRepo.save(message);
 
